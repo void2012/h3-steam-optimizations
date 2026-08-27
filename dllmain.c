95b0f30dcde46f95fab41665ee4f0ded892f60f3
devoider17 <heroerror3@gmail.com>
2026-08-28 05:32:09 +0700

    Update dllmain.c

--- dllmain.c
+++ dllmain.c
@@ -9,15 +9,22 @@ static void WriteJump(uintptr_t address, void* func)
 	// 0xAABBCCDD function address
 	// 0xC3 ret
 	uint8_t buffer[6] = { 0x68, 0, 0, 0, 0, 0xC3 };
-	*(uint32_t*)&buffer[1] = (uint32_t)func;
+	*(uintptr_t*)&buffer[1] = (uintptr_t)func;
 	DWORD prev;
 	VirtualProtect((void*)(address), sizeof(buffer), PAGE_EXECUTE_READWRITE, &prev);
-	memcpy((void*)address, &buffer[0], sizeof(buffer));
+	memcpy((void*)(address), &buffer[0], sizeof(buffer));
 	VirtualProtect((void*)(address), sizeof(buffer), prev, nullptr);
 }
 
+extern void __fastcall Process1WindowsMessage();
+extern void __fastcall GameTime__DelayTil(DWORD);
+extern void __fastcall GameTime__Delay(DWORD);
+
 __declspec(dllexport) void rpmalloc_patches_initialize()
 {
+	WriteJump(0x4F7FB0, &Process1WindowsMessage); // Process1WindowsMessage
+	WriteJump(0x4F82F0, &GameTime__DelayTil); // GameTime::DelayTil
+	WriteJump(0x4F83C0, &GameTime__Delay); // GameTime::Delay
 	WriteJump(0x61A405, &rpmalloc); // malloc
 	WriteJump(0x616ED2, &rpmalloc); // operator new
 	WriteJump(0x6195E0, &rpfree); // free
