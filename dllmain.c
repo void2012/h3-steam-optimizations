15c1c7ac7090b801bc79619c2b0c4dc3897a8bf7
devoider17 <heroerror3@gmail.com>
2026-08-31 10:36:15 +0700

    Update dllmain.c

--- dllmain.c
+++ dllmain.c
@@ -22,16 +22,18 @@ extern void __fastcall GameTime__Delay(DWORD);
 
 __declspec(dllexport) void rpmalloc_patches_initialize()
 {
+	// CPU utilization
 	WriteJump(0x4F7FB0, &Process1WindowsMessage); // Process1WindowsMessage
 	WriteJump(0x4F82F0, &GameTime__DelayTil); // GameTime::DelayTil
 	WriteJump(0x4F83C0, &GameTime__Delay); // GameTime::Delay
+	// Optimized heap
 	WriteJump(0x61A405, &rpmalloc); // malloc
 	WriteJump(0x616ED2, &rpmalloc); // operator new
 	WriteJump(0x6195E0, &rpfree); // free
 	WriteJump(0x60AB30, &rpfree); // operator delete
 	WriteJump(0x61A491, &rpcalloc); // calloc
 	WriteJump(0x6192C0, &rprealloc); // realloc
-	WriteJump(0x61DF44, &rpmalloc_usable_size); // resize
+	WriteJump(0x61DF44, &rpmalloc_usable_size); // _msize
 }
 
 extern __declspec(dllexport) BOOL WINAPI
